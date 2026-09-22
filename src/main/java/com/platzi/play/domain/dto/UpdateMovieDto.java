3919f3d496072baab938f8dcb647ea9f2298f634
juan-latorre <juanlatorrevidal@gmail.com>
2026-09-22 16:11:29 -0500

    Se implementa validaciones con org.springframework.boot:spring-boot-starter-validation y anotaciones en UpdateMovieDto(NotBlank,PastOrPresent, Min y Max

--- src/main/java/com/platzi/play/domain/dto/UpdateMovieDto.java
+++ src/main/java/com/platzi/play/domain/dto/UpdateMovieDto.java
@@ -1,10 +1,18 @@
 package com.platzi.play.domain.dto;
 
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
 public record UpdateMovieDto(
+        @NotBlank(message = "El título no puede estar vacío")
         String title,
+
+        @PastOrPresent(message = "La fecha de lanzamiento debe ser anterior o igual a la fecha actual")
         LocalDate releaseDate,
+
+        @Min(value = 0, message = "La calificación no puede ser negativa")
+        @Max(value = 10, message = "La calificación no puede ser mayor a 10")
         Double rating
 ) {
 
--- src/main/java/com/platzi/play/web/exception/RestExceptionHandler.java
+++ src/main/java/com/platzi/play/web/exception/RestExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.platzi.play.web.exception;
 
 import com.platzi.play.domain.exception.MovieAlreadyExistException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -13,6 +16,24 @@ public class RestExceptionHandler {
     public ResponseEntity<Error> handleException(MovieAlreadyExistException ex) {
         Error error = new Error("movie-already-exist", ex.getMessage());
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handleException(MethodArgumentNotValidException ex) {
+
+        List<Error> errors = new ArrayList<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+             errors.add(new Error(error.getField(), error.getDefaultMessage()));
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(Exception ex) {
+        Error error = new Error("unexpected-error", ex.getMessage());
+        return ResponseEntity.internalServerError().body(error);
     }
 
 }
